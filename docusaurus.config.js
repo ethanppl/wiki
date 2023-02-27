f11023842967609d5169b05e24a477b445b3dcca
Ethan Pang <ethan.pang@gitstart.com>
2023-02-27 13:39:06 +0800

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
+  title: "Ethan's Wiki",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
@@ -55,9 +55,9 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Wiki",
+        title: "My Personal Wiki",
         logo: {
-          alt: "My Site Logo",
+          alt: "My Wiki Logo",
           src: "img/logo.svg",
         },
         items: [
